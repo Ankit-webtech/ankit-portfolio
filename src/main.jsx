be2a1909f61a1dfab15988d53500bc9c23dae7df
[Ankit Nagar] <[iamankitnagar7@gmail.com]>
2025-09-21 10:24:10 +0530

    your message

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client';
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import App from './App.jsx'
-import './index.css'   // <- यह बहुत जरूरी है
+import './index.css'  
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
