242c309766f037a90461db022fb6b521ef20f9a9
Ankit nagar <iamankitnagar7@gmail.com>
2026-02-11 01:03:40 -0800

    Update resume link in Home component

--- src/Home.jsx
+++ src/Home.jsx
@@ -87,7 +87,7 @@ export default function Home() {
 
           {/* Resume Button */}
           <a
-            href="/Ankit-resume-2026(1).pdf"
+            href="/Ankit-Nagar-Resume-3-26.pdf"
             target="_blank"
             rel="noopener noreferrer"
             className="px-10 py-2 text-cyan-500 text-lg rounded-full shadow-lg transition-transform transform bg-transparent border-2 border-white hover:scale-105
